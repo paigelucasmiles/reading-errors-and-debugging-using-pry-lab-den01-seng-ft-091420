ecd860229b57f2a1597bedef41ba70dafed3ee5e
Paige Miles <paige@lucas-miles.com>
2020-09-01 20:22:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,10 +11,11 @@ def prompt_user
   puts "2.) Eat a ham."
 end
 
-def selection(get_user_input)
-  if get_user_input = 1
+def selection(num)
+  if num = 1
+    binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif get_user_input = 2
+  elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
