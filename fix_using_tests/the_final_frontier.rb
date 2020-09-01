53d50f876a444aba0292064d0b99005d2779e649
Paige Miles <paige@lucas-miles.com>
2020-09-01 20:54:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,6 +3,7 @@ require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
+  binding.pry
 end
 
 
@@ -11,6 +12,6 @@ def state_log(start_date)
 end
 
 def engage
-  binding.pry puts state_log(date)
+  puts state_log(date)
   date = generate_star_date
 end
